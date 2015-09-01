fe30921f4cd7b3a5578cd9c76e94857359c3a351
杨小兵 <six_tiger@163.com>
2015-09-01 11:24:13 +0800

    优化代码

--- XXBLib/Category/NSString+Help.m
+++ XXBLib/Category/NSString+Help.m
@@ -155,8 +155,6 @@
  */
 - (BOOL)isValidEmail
 {
-    NSString *emailRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
-    NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
-    return [emailTest evaluateWithObject:self];
+    return [[NSPredicate predicateWithFormat:@"SELF MATCHES [A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"] evaluateWithObject:self];
 }
 @end
