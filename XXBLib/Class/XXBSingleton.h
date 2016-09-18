98c45bbdd7915e8d9c1540a9a78381c901b0d5f1
yangxiaobing <lhxiaobing@qq.com>
2016-09-18 22:09:34 +0800

    解决某个文件包含多个类的情况下单例出现问题的情况

--- XXBLib/Class/XXBSingleton.h
+++ XXBLib/Class/XXBSingleton.h
@@ -8,27 +8,27 @@
 
 // .m文件
 #define XXBSingletonM(name) \
-static id _instance = nil; \
+static id _instance##name = nil; \
 + (id)allocWithZone:(struct _NSZone *)zone\
 {\
-    if (_instance == nil)\
+    if (_instance##name == nil)\
     {\
         static dispatch_once_t onceToken;\
         dispatch_once(&onceToken, ^{\
-            _instance = [super allocWithZone:zone];\
+            _instance##name = [super allocWithZone:zone];\
         });\
     }\
-    return _instance;\
+    return _instance##name;\
 }\
 + (instancetype)shared##name \
 { \
     static dispatch_once_t onceToken; \
     dispatch_once(&onceToken, ^{ \
-        _instance = [[self alloc] init]; \
+        _instance##name = [[self alloc] init]; \
     }); \
-    return _instance; \
+    return _instance##name; \
 } \
 - (id)copyWithZone:(NSZone *)zone \
 { \
-    return _instance; \
+    return _instance##name; \
 }
